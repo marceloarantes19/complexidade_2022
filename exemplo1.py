33de93def589a74c19593490f6d6ca832ce3fbbf
Marcelo Arantes <marceloarantes19@gmail.com>
2022-10-10 18:59:37 -0300

    Aula de 10 de Outubro - 2022

---
+++ exemplo1.py
@@ -0,0 +1,17 @@
+# Dada uma string s, retorne a letra de maior frequência (apenas alfabéticas) em s
+# Exemplo 1: 
+#   Entrada: s = 'abcdacddda111112311139dd'
+#   Saída: 'd'  
+# Exemplo 2: 
+#   Entrada: s = 'AAABCDd@#$11112#49ddBBBZBBB'
+#   Saída: 'B' 
+# Altere apenas a função solucao
+
+def solucao(s):
+  # Escreva a solução aqui
+  l = ""
+  return l 
+
+# Não modifique o código abaixo
+s = input('Digite um texto: ')
+print('Letra de maior frequência: ', solucao(s))
